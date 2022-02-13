cb50fc97fdc3d29dcbe25478c034dd691fb6aafa
shuvomh2000 <shuvomh2000@gmail.com>
2022-02-13 23:25:38 +0600

    user only

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+import { useState,useEffect } from "react";
+import { Card,Button, Container, Row, Col,ListGroup } from 'react-bootstrap';
+
+
+
+
 
 function App() {
+  const [userdata,setUserdata] = useState([])
+
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/users").then((user)=>{
+      setUserdata(user.data)
+    }).catch((error)=>{
+      console.log(error)
+    })
+
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container>
+    <Row>
+      {userdata.map((item)=>(
+        <Col lg={4}>
+        <Card>
+    <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPZob-roOCV5Q54sF9k0oRrvRZ846yMMAtJg&usqp=CAU" />
+    
+
+    <Card.Body>
+      <Card.Title>{item.name} ({item.username})</Card.Title>
+            <ListGroup>
+        <ListGroup.Item><b>email:</b> {item.email}</ListGroup.Item>
+        <ListGroup.Item><b>address:</b> {item.address.street}, {item.address.suite}, {item.address.city} ({item.address.zipcode} )</ListGroup.Item>
+      </ListGroup>
+      <Button className='mt-5' variant="primary">{item.id} Read more</Button>
+    </Card.Body>
+  </Card>
+        </Col>
+      ))}
+    </Row>
+</Container>
+
+    </>
   );
 }
 
